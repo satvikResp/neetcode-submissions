3c833d6227b5011fa8315d912bb5a3deca40afee
satvikResp <satvikgautam5@gmail.com>
2026-06-02 19:05:03 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkpalindrome(string s,int strt,int endi){
+        while(strt<endi){
+            if(s[strt]!=s[endi]){
+                return false;
+            }
+            strt++;
+            endi--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return checkpalindrome(s,i+1,j)||checkpalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
