09b82fba251a6d1ab6b79d0b0f04ba66a39af85e
satvikResp <satvikgautam5@gmail.com>
2026-08-07 08:38:17 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode *check(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==nullptr) return nullptr;
+
+        if(root->val>=p->val &&root->val<=q->val) return root;
+        if(root->val>p->val &&root->val>q->val){
+            return check(root->left,p,q);
+        }
+        else{
+            return check(root->right,p,q);
+        }
+        return nullptr;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val > q->val)
+        swap(p, q);
+        return check(root,p,q);
+    }
+};
