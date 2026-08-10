6f95715bc6509b7a94833e2415d567f3b8e95b72
satvikResp <satvikgautam5@gmail.com>
2026-08-10 17:19:16 +0530

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+    
+   
+    void preorder(TreeNode *root,string &encoded){
+        if(root==nullptr) {
+           
+            encoded+='#';
+            encoded+=',';
+            return;
+        }
+        
+        encoded+=to_string(root->val);
+        encoded+=',';
+        preorder(root->left,encoded);
+        preorder(root->right,encoded);
+        return;
+    }
+    string serialize(TreeNode* root) {
+        string encoded;
+        preorder(root,encoded);
+        // encoded.erase(encoded.size()-1,1);
+        return encoded;
+    }
+
+    
+    TreeNode* attach(vector<int>& pre, int& i) {
+
+    if (pre[i] == -1001){
+        i++;
+        return nullptr;
+    }
+    TreeNode* root = new TreeNode(pre[i]);
+    i++;
+
+    root->left = attach(pre, i);
+    root->right = attach(pre, i);
+
+    return root;
+}
+
+    TreeNode* deserialize(string data) {
+        string curr;
+        stringstream ss(data);
+        vector<int>decoded;
+        while(getline(ss,curr,',')){
+            if(curr=="#") {
+            decoded.push_back(-1001);
+            continue;
+            }
+            decoded.push_back(stoi(curr));
+        }
+        int i=0;
+        return attach(decoded,i);
+
+    }
+};
