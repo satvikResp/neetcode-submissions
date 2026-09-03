412c0938dbb905bd5cc006838efa07713d8765fe
satvikResp <satvikgautam5@gmail.com>
2026-09-03 21:15:35 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void func(vector<int>& nums,vector<vector<int>>&ans,vector<int>&subset,int i){
+        if(i==nums.size()){
+            ans.push_back(subset);
+            return;
+        }
+        for(int idx=i;idx<nums.size();idx++){
+        subset.push_back(nums[i]);
+        func(nums,ans,subset,idx+1);
+        subset.pop_back();
+        func(nums,ans,subset,idx+1);
+        return;
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        int i=0;
+        func(nums,ans,subset,i);
+        return ans;
+    }
+};
