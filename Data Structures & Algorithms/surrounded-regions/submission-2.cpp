b2fd77b4849d6e9b9aee4f5bb11fdb08b082115a
satvikResp <satvikgautam5@gmail.com>
2026-09-16 18:26:34 +0530

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
+    void dfs(vector<vector<char>>& board,int row ,int col,vector<vector<bool>>&visited){
+
+      visited[row][col]=true;
+
+       for(auto dir :directions){
+        int r=row+dir[0];
+        int c=col+dir[1];
+
+        if(r>=0 && r<board.size() && c>=0 && c<board[0].size() && board[r][c]=='O' && !visited[r][c]){
+            dfs(board,r,c,visited);
+        }
+       }
+
+
+    }
+    void solve(vector<vector<char>>& board) {
+        //sirf boundary wale O dekh and jo unse connected hai unko true krde aur baki sare false reh jayenge aur unko phir x bana dege
+        vector<vector<bool>>visited(board.size(),vector<bool>(board[0].size(),false));
+        //top wali boundary  row=0 and col 1 2 3 4 5 
+        for(int i=0;i<board[0].size();i++){
+            if(board[0][i]=='O'){
+                dfs(board,0,i,visited);
+            }
+        }
+        //left boundary row=1 2 3 4 5 and col =0
+        for(int i=0;i<board.size();i++){
+            if(board[i][0]=='O'){
+                dfs(board,i,0,visited);
+            }
+        }
+
+        //bottom boundary row=last row and col 1 2 3 4 5 
+        for(int i=0;i<board[0].size();i++){
+            if(board[board.size()-1][i]=='O'){
+                dfs(board,board.size()-1,i,visited);
+            }
+        }
+
+        //right boundary row 1 2 3 4 5 and col last col
+        // right boundary
+        for(int i=0;i<board.size();i++){
+            if(board[i][board[0].size()-1]=='O'){
+                dfs(board,i,board[0].size()-1,visited);
+            }
+        }
+
+
+        //ab false walon ko x banado agar nhi hai to 
+
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(visited[i][j]==false && board[i][j]!='X'){
+                    board[i][j]='X';
+                }
+            }
+        }
+
+    }
+};
